298e9004e4b33e3a4edb2d0f78572b4df514dc09
João Martins <joao@sousamartins.com>
2018-03-25 13:46:53 +0100

    initial setup

--- pac-masternode-setup/setup_all.sh
+++ pac-masternode-setup/setup_all.sh
@@ -3,26 +3,30 @@
 ### installing Pac
 
 sudo apt-get update
-sudo apt-get -y --allow-unauthenticated install software-properties-common
-sudo apt-get -y --allow-unauthenticated install dirmngr --install-recommends
-sudo apt-get -y --allow-unauthenticated install libminiupnpc-dev
-sudo apt-get -y --allow-unauthenticated install libboost-all-dev 
-sudo apt-get -y --allow-unauthenticated install build-essential
-sudo apt-get -y --allow-unauthenticated install libtool
-sudo apt-get -y --allow-unauthenticated install autotools-dev
-sudo apt-get -y --allow-unauthenticated install automake pkg-config
-sudo apt-get -y --allow-unauthenticated install libssl-dev
-sudo apt-get -y --allow-unauthenticated install libevent-dev bsdmainutils
-sudo apt-get -y --allow-unauthenticated install git
-sudo apt-get -y --allow-unauthenticated install libzmq3-dev
-sudo apt-get -y --allow-unauthenticated install libdb4.8-dev
-sudo apt-get -y --allow-unauthenticated install libdb4.8++-dev
-sudo apt-get -y --allow-unauthenticated install virtualenv
-sudo apt-get -y --allow-unauthenticated install ufw
-sudo apt-get -y --allow-unauthenticated install pwgen
-sudo apt-get -y --allow-unauthenticated install jq
-sudo add-apt-repository ppa:git-core/ppa
+sudo apt-get -y --allow-unauthenticated install --install-recommends software-properties-common 
+sudo apt-get -y --allow-unauthenticated install --install-recommends libminiupnpc-dev
+sudo apt-get -y --allow-unauthenticated install --install-recommends libboost-all-dev
+sudo apt-get -y --allow-unauthenticated install --install-recommends build-essential
+sudo apt-get -y --allow-unauthenticated install --install-recommends autotools-dev
+sudo apt-get -y --allow-unauthenticated install --install-recommends automake
+sudo apt-get -y --allow-unauthenticated install --install-recommends pkg-config 
+sudo apt-get -y --allow-unauthenticated install --install-recommends libssl-dev 
+sudo apt-get -y --allow-unauthenticated install --install-recommends libevent-dev 
+sudo apt-get -y --allow-unauthenticated install --install-recommends bsdmainutils 
+sudo apt-get update
+sudo apt-get -y --allow-unauthenticated install --install-recommends git 
+
+sudo apt-get -y --allow-unauthenticated install --install-recommends dirmngr
+sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
+sudo apt-get -y --allow-unauthenticated install --install-recommends libzmq3-dev 
+sudo apt-get update
+sudo apt-get -y --allow-unauthenticated install --install-recommends libdb4.8-dev libdb4.8++-dev  
+sudo apt-get -y --allow-unauthenticated install --install-recommends virtualenv 
+sudo apt-get -y --allow-unauthenticated install --install-recommends ufw 
+sudo apt-get -y --allow-unauthenticated install --install-recommends pwgen 
+sudo apt-get -y --allow-unauthenticated install --install-recommends jq
+
 
 wget https://raw.githubusercontent.com/PACCommunity/PAC/master/pacmn.sh
 chmod +x pacmn.sh
@@ -32,3 +36,4 @@ wget https://github.com/nomukaiki/paccoin-config/raw/master/pac-masternode-setup
 tar xzvf pac-masternode-setup.tar.gz
 cd pac-masternode-setup
 ./setup_supervisor.sh gisan nomukaiki!
+
