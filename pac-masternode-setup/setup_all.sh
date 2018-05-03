efa49eb2f0cb55c3dfb9f7cace1e8d1193f06b14
João Martins <joao@sousamartins.com>
2018-05-03 20:45:16 +0100

    Upgraded version

--- pac-masternode-setup/setup_all.sh
+++ pac-masternode-setup/setup_all.sh
@@ -4,105 +4,125 @@ set -e
 
 sudo apt-get update
 sudo apt-get -y --allow-unauthenticated install --install-recommends software-properties-common 
-sudo apt-get -y --allow-unauthenticated install --install-recommends libminiupnpc-dev
-sudo apt-get -y --allow-unauthenticated install --install-recommends libboost-all-dev build-essential autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils    
-sudo apt-get -y --allow-unauthenticated install --install-recommends bsdmainutils 
-sudo apt-get -y --allow-unauthenticated install --install-recommends git dirmngr libzmq3-dev
-sudo add-apt-repository -y ppa:git-core/ppa
-sudo add-apt-repository -y ppa:bitcoin/bitcoin
-sudo apt-get update
-sudo apt-get -y --allow-unauthenticated install --install-recommends libdb4.8-dev libdb4.8++-dev  virtualenv ufw pwgen jq
+sudo apt-get -y --allow-unauthenticated install --install-recommends git python virtualenv pwgen
+
+arch=`uname -m`
+version="0.12.3.1"
+old_version="0.12.3.0"
+base_url="https://github.com/PACCommunity/PAC/releases/download/v${version}"
+if [ "${arch}" == "x86_64" ]; then
+	tarball_name="PAC-v${version}-linux-x86_64.tar.gz"
+	binary_url="${base_url}/${tarball_name}"
+elif [ "${arch}" == "x86_32" ]; then
+	tarball_name="PAC-v${version}-linux-x86.tar.gz"
+	binary_url="${base_url}/${tarball_name}"
+else
+	echo "PAC binary distribution not available for the architecture: ${arch}"
+	exit -1
+fi
 
 echo "################################################"
-echo "#   Welcome to PAC Masternode's server setup   #"
+echo "#   Welcome to PAC Masternode's server setup   #"		
 echo "################################################"
 echo "" 
 read -p 'Please provide the external IP: ' ipaddr
 read -p 'Please provide masternode genkey: ' mnkey
 
 while [[ $ipaddr = '' ]] || [[ $ipaddr = ' ' ]]; do
-        read -p 'You did not provided an external IP, please provide one: ' ipaddr
-        sleep 2
+	read -p 'You did not provided an external IP, please provide one: ' ipaddr
+	sleep 2
 done
 
 while [[ $mnkey = '' ]] || [[ $mnkey = ' ' ]]; do
-        read -p 'You did not provided masternode genkey, please provide one: ' mnkey
-        sleep 2
+	read -p 'You did not provided masternode genkey, please provide one: ' mnkey
+	sleep 2
 done
 
 echo "###############################"
-echo "#  Installing Dependencies    #"
+echo "#  Installing Dependencies    #"		
 echo "###############################"
 echo ""
-echo "Running this script on Ubuntu 16.04 LTS is highly recommended (2GB RAM)"
+echo "Running this script on Ubuntu 16.04 LTS or newer is highly recommended."
+
+sudo apt-get -y update
+sudo apt-get -y install git python virtualenv ufw pwgen 
+
 
 echo "###############################"
-echo "#   Setting up the Firewall   #"
+echo "#   Setting up the Firewall   #"		
 echo "###############################"
 sudo ufw status
 sudo ufw disable
 sudo ufw allow ssh/tcp
 sudo ufw limit ssh/tcp
-sudo ufw allow 7112/tcp
+sudo ufw allow $pac_port/tcp
 sudo ufw logging on
 sudo ufw --force enable
 sudo ufw status
 
-sudo iptables -A INPUT -p tcp --dport '7112' -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport $pac_port -j ACCEPT
 
 echo ""
 echo "###############################"
-echo "#      Get/Setup binaries     #"
+echo "#      Get/Setup binaries     #"		
 echo "###############################"
 echo ""
-wget "https://github.com/PACCommunity/PAC/releases/download/v0.12.3.0/PAC-v0.12.3.0-linux-x86_64.tar.gz"
-tar xvf 'PAC-v0.12.3.0-linux-x86_64.tar.gz'
-cd ~/
-rm PAC-v0.12.3.0-linux-x86_64.tar.gz
-currpath=$( pwd )
-echo "Binaries got stored on: $currpath"
-chmod +x paccoind
-chmod +x paccoin-cli
+
+if test -e "${tarball_name}"; then
+	rm -r $tarball_name
+fi
+wget $binary_url
+if test -e "${tarball_name}"; then
+	echo "Unpacking $PAC distribution"
+	tar -xvzf $tarball_name
+	chmod +x paccoind
+	chmod +x paccoin-cli
+	echo "Binaries were saved to: $PWD/$tarball_name"
+	rm -r $tarball_name
+else
+	echo "There was a problem downloading the binaries, please try running again the script."
+	exit -1
+fi
 
 echo "###############################"
-echo "#     Configure the wallet    #"
+echo "#     Configure the wallet    #"		
 echo "###############################"
 echo ""
 echo "The .paccoincore folder will be created, if folder already exists, it will be replaced"
 if [ -d ~/.paccoincore ]; then
-        if [ -e ~/.paccoincore/paccoin.conf ]; then
-                read -p "The file paccoin.conf already exists and will be replaced. do you agree [y/n]:" cont
-            	if [ $cont = 'y' ] || [ $cont = 'yes' ] || [ $cont = 'Y' ] || [ $cont = 'Yes' ]; then
-                        sudo rm ~/.paccoincore/paccoin.conf
-                        touch ~/.paccoincore/paccoin.conf
-                        cd ~/.paccoincore
-                fi
-        fi
+	if [ -e ~/.paccoincore/paccoin.conf ]; then
+		read -p "The file paccoin.conf already exists and will be replaced. do you agree [y/n]:" cont
+		if [ $cont = 'y' ] || [ $cont = 'yes' ] || [ $cont = 'Y' ] || [ $cont = 'Yes' ]; then
+			sudo rm ~/.paccoincore/paccoin.conf
+			touch ~/.paccoincore/paccoin.conf
+			cd ~/.paccoincore
+		fi
+	fi
 else
-        echo "Creating .paccoincore dir"
-        mkdir -p ~/.paccoincore
-        cd ~/.paccoincore
-        touch paccoin.conf
+	echo "Creating .paccoincore dir"
+	mkdir -p ~/.paccoincore
+	cd ~/.paccoincore
+	touch paccoin.conf
 fi
 
 echo "Configuring the paccoin.conf"
 echo "rpcuser=$(pwgen -s 16 1)" > paccoin.conf
 echo "rpcpassword=$(pwgen -s 64 1)" >> paccoin.conf
 echo "rpcallowip=127.0.0.1" >> paccoin.conf
-echo "rpcport=7111" >> paccoin.conf
+echo "rpcport=$pac_rpc_port" >> paccoin.conf
 echo "externalip=$ipaddr" >> paccoin.conf
-echo "port=7112" >> paccoin.conf
+echo "port=$pac_port" >> paccoin.conf
 echo "server=1" >> paccoin.conf
 echo "daemon=1" >> paccoin.conf
 echo "listen=1" >> paccoin.conf
-echo "staking=0" >> paccoin.conf
+echo "testnet=$is_testnet" >> paccoin.conf
 echo "masternode=1" >> paccoin.conf
-echo "masternodeaddr=$ipaddr:7112" >> paccoin.conf
+echo "masternodeaddr=$ipaddr:$pac_port" >> paccoin.conf
 echo "masternodeprivkey=$mnkey" >> paccoin.conf
 
 
 echo "###############################"
-echo "#      Running the wallet     #"
+echo "#      Running the wallet     #"		
 echo "###############################"
 echo ""
 cd ~/
@@ -110,16 +130,14 @@ cd ~/
 ./paccoind
 sleep 60
 
-syncedinfo=$( ./paccoin-cli mnsync status )
-assetid=$( echo $syncedinfo | jq '.AssetID' )
-
-if [ $assetid == 'null' ]; then
-        echo "Wallet is not running or there is an issue, please restart wallet!"
-        exit
+is_pac_running=`ps ax | grep -v grep | grep paccoind | wc -l`
+if [ $is_pac_running -eq 0 ]; then
+	echo "The daemon is not running or there is an issue, please restart the daemon!"
+	exit
 fi
 
 echo "###############################"
-echo "#     Running the sentinel    #"
+echo "#     Running the sentinel    #"		
 echo "###############################"
 echo ""
 git clone "https://github.com/PACCommunity/sentinel"
@@ -140,6 +158,6 @@ echo "Don't forget to run the masternode from your cold wallet!"
 wget https://github.com/nomukaiki/paccoin-config/raw/master/pac-masternode-setup.tar.gz
 tar xzvf pac-masternode-setup.tar.gz
 cd pac-masternode-setup
-./setup_supervisor.sh gisan nomukaiki!
-sudo service supervisor restart
+echo "Now run the supervisor setup manually and enter your username+password: ./setup_supervisor.sh <username> <password>" 
+
 
